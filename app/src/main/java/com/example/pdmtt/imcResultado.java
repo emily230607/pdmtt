fd2c15aecdcb376e816c7d1447cc706ac02d962d
Emily <emily.s23@aluno.ifsc.edu.br>
2025-09-08 09:13:01 -0300

    Imc Calculadora

--- app/src/main/java/com/example/pdmtt/imcResultado.java
+++ app/src/main/java/com/example/pdmtt/imcResultado.java
@@ -1,8 +1,6 @@
 package com.example.pdmtt;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,21 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button button;
+public class imcResultado extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        button.setOnClickListener(v ->{
-            Intent intent = new Intent(this, imcResultado.class);
-        startActivity(intent);
-
-        } );
-
+        setContentView(R.layout.activity_imc_resultado);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
