226582626f56163d23bc08fb819cffbbd2cc1a39
emily <emily.s23@aluno.ifsc.edu.br>
2025-09-15 09:16:46 -0300

    imc aula 2

--- app/src/main/java/com/example/pdmtt/MainActivity.java
+++ app/src/main/java/com/example/pdmtt/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pdmtt;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,15 +14,26 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    EditText edPeso, edAltura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button4);
+        edPeso = findViewById(R.id.edPeso);
+        edAltura = findViewById(R.id.edAltura);
         button.setOnClickListener(v ->{
-            Intent intent = new Intent(this, imcResultado.class);
-        startActivity(intent);
+
+            Intent i = new Intent(MainActivity.this, imcResultado.class);
+            Bundle bundle = new Bundle();
+            Double peso = Double.parseDouble((edPeso.getText().toString()) );
+            Double altura = Double.parseDouble((edAltura.getText().toString() ));
+            bundle.putDouble("peso", peso);
+            bundle.putDouble("altura", altura);
+            i.putExtras(bundle);
+        startActivity(i);
 
         } );
 
--- app/src/main/java/com/example/pdmtt/imcResultado.java
+++ app/src/main/java/com/example/pdmtt/imcResultado.java
@@ -1,24 +1,38 @@
 package com.example.pdmtt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class imcResultado extends AppCompatActivity {
+
+    TextView tvResultado;
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_imc_resultado);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        tvResultado = findViewById(R.id.tvResultado);
+        imageView = findViewById(R.id.imageViewPerfil);
+
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        Double peso = b.getDouble("peso");
+        Double altura = b.getDouble("altura");
+
+        Double imc = peso/(altura*altura);
+
+        tvResultado.setText(Double.toString(imc));
+        imageView.setImageResource(R.drawable.perfil);
+
+
+
     }
 }
