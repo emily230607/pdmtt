c8070ad3a956d3df0deee2adafe8a69e48e36454
Emily <emily.s23@aluno.ifsc.edu.br>
2025-11-03 08:55:38 -0300

    Banco de Dados

--- app/src/main/java/com/example/pdmtt/MainActivity.java
+++ app/src/main/java/com/example/pdmtt/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.pdmtt;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    SQLiteDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        db = openOrCreateDatabase("app_database", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE notas (id INTEGER PRIMARY KEY AUTOINCREMENT," + "TITULO varchar, texto TEXT)");
+
+        ContentValues values = new ContentValues();
+        values.put("chega Natal hohoho", "Minha primeira nota");
+        values.put("Texto", "esse é o texyo suohrg");
+        db.insert("notas", null, values);
+
     }
 }
