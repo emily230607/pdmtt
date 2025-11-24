5de68127f746464ad6ee5efcdfe2b643d66ea202
Emily <emily.s23@aluno.ifsc.edu.br>
2025-11-24 08:52:56 -0300

    Sensor 1

--- app/src/main/java/com/example/pdmtt/MainActivity.java
+++ app/src/main/java/com/example/pdmtt/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.pdmtt;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sm;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        textView= findViewById(R.id.textViewSensor);
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sm.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    public void onSensorChanged(SensorEvent event) {
+        event.sensor.getName();
+        textView.setText(Float.toString(event.values[0]));
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    }
+
 }
