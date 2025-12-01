0930a0bc7ecd9490cdf1113b8a82a9187ab89f35
Emily <emily.s23@aluno.ifsc.edu.br>
2025-12-01 08:17:41 -0300

    sorteio

--- app/src/main/java/com/example/pdmtt/MainActivity.java
+++ app/src/main/java/com/example/pdmtt/MainActivity.java
@@ -1,71 +1,70 @@
 package com.example.pdmtt;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button button; //aponta inicialmente para null
     EditText editTextMin, editTextMax;
     TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        button=findViewById(R.id.button);
-        editTextMin=findViewById(R.id.edMin);
-        editTextMax=findViewById(R.id.edMax);
+        button =findViewById(R.id.button);
+        editTextMin =findViewById(R.id.edMin);
+        editTextMax =findViewById(R.id.edMax);
+
+        editTextMin.setText(Integer.toString(50));
+        editTextMax.setText(Integer.toString(100));
         tv=findViewById(R.id.tvResultado);
 
-        button.setOnClickListener(v ->{
-
-            Random random = new Random();
-
+        button.setOnClickListener(v -> {
+            Random  random=new Random();
             int min,max;
             min=Integer.parseInt(editTextMin.getText().toString());
             max=Integer.parseInt(editTextMax.getText().toString());
-            int delta= max- min;
+            int delta=max-min;
+            int sortiado= random.nextInt(delta)+min+1;
 
-            int sortiado= random.nextInt(delta)+min;
-            tv.setText(Integer.toString(sortiado));
+            tv.setText(Integer.toString( sortiado ));
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    /// protect  OnSaveInstaceState ele é chamado na recriação do layout no ciclo de vida
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("sorteado",tv.getText().toString() );
+    }
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState!=null){
+            tv.setText(savedInstanceState.getString("sorteado"));
+        }
+    }
+    /// protect  OnSaveInstaceState ele é chamado após um onDestroy
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.wtf("ciclo de vida", "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.wtf("ciclo de vida", "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.wtf("ciclo de vida", "onResume");
+    public void onRestoreInstanceState(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
     }
 }
